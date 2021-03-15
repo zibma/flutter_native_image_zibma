8f3e385c7581191048f5a4bbc27051940cbed733
Ankur <ankur@zibma.com>
2021-03-15 15:03:58 +0530

    Init Upload

---
+++ android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
@@ -0,0 +1,44 @@
+package com.example.flutternativeimage;
+
+import android.content.Context;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry;
+
+/**
+ * FlutterNativeImagePlugin
+ */
+public class FlutterNativeImagePlugin implements FlutterPlugin {
+  private static final String CHANNEL_NAME = "flutter_native_image";
+  private MethodChannel channel;
+  /**
+   * Plugin registration.
+   */
+  public static void registerWith(PluginRegistry.Registrar registrar) {
+    final FlutterNativeImagePlugin plugin = new FlutterNativeImagePlugin();
+    plugin.setupChannel(registrar.messenger(), registrar.context());
+  }
+
+  @Override
+  public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
+    setupChannel(binding.getFlutterEngine().getDartExecutor(), binding.getApplicationContext());
+  }
+
+  @Override
+  public void onDetachedFromEngine(FlutterPlugin.FlutterPluginBinding binding) {
+    teardownChannel();
+  }
+
+  private void setupChannel(BinaryMessenger messenger, Context context) {
+    channel = new MethodChannel(messenger, CHANNEL_NAME);
+    MethodCallHandlerImpl handler = new MethodCallHandlerImpl(context);
+    channel.setMethodCallHandler(handler);
+  }
+
+  private void teardownChannel() {
+    channel.setMethodCallHandler(null);
+    channel = null;
+  }
+}
